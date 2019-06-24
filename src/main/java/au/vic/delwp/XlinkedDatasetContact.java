0a1b21e374f25bbc3425380f7948eec1e3a89590
Simon Pigot <Simon.Pigot@csiro.au>
2019-06-24 16:17:11 +0200

    Update service name used to retrieve fragments

--- src/main/java/au/vic/delwp/XlinkedDatasetContact.java
+++ src/main/java/au/vic/delwp/XlinkedDatasetContact.java
@@ -12,7 +12,7 @@ public class XlinkedDatasetContact {
   public String getRole() { return ds.role.getRole(); }
 
 	public String getContactXlink(){
-    return "local://xml.metadata.get?uuid="+Utils.generateContactUUID(ds.contact.getContactID()); 
+    return "local://srv/api/registries/entries/"+Utils.generateContactUUID(ds.contact.getContactID()); 
 	}
 
 	}
