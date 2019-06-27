c0e511a47c9b3a90b1798201435117a7d19e73a4
Simon Pigot <Simon.Pigot@csiro.au>
2019-06-27 11:02:48 +0200

    Fix for #11

--- src/main/java/au/vic/delwp/Dataset.java
+++ src/main/java/au/vic/delwp/Dataset.java
@@ -465,8 +465,8 @@ public class Dataset {
       for (int i = 0; i < resources.size(); i++) {
          AssociatedResource assRes = new AssociatedResource();
          assRes.anzlicId = (String)resources.get(i);
-         assRes.UUID = UUID;
-         assRes.title = getTitle();
+         assRes.UUID = DigestUtils.sha1Hex(assRes.anzlicId);
+         assRes.title = "Component dataset "+assRes.UUID;
          assRes.hostNameForLinks = hostNameForLinks;
          associatedResources.add(assRes);
       }
